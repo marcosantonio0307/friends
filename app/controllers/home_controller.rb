8267e7209722d7f9228953d502447e6fb53f8889
Marcos Antonio <marcosantoniotaina@gmail.com>
2018-11-30 23:39:38 -0300

    Inserido a opcao de curtir

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
 
   def index
   end
-
+  
+  def curtir
+  	id = params[:id]
+  	publicacao = Publication.find id
+  	publicacao.update(like: publicacao.like+1)
+   	redirect_to root_url
+  end
 end
